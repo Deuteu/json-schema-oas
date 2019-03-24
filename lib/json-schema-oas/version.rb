52f7213e46fe57a44b4c6ea3ee86e29aafea88e8
Deuteu <Deuteu@users.noreply.github.com>
2019-03-25 00:18:38 +0100

    Init gem.

---
+++ lib/json-schema-oas/version.rb
@@ -0,0 +1,7 @@
+module JSON
+  module Oas
+    class Schema
+      VERSION = '0.1.0'.freeze
+    end
+  end
+end
---
+++ spec/json-schema-oas/oas_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe JSON::Oas::Schema do
+  it 'has a version number' do
+    expect(JSON::Oas::Schema::VERSION).not_to be nil
+  end
+end
